cf43e99012075365ad585bae03e1f40071adb406
Eric Whitehead <ericpwhitehead@gmail.com>
2018-10-09 14:20:02 -0400

    first

--- app/models/api.js
+++ app/models/api.js
@@ -35,7 +35,18 @@ var projectSchema = mongoose.Schema({
 });
 const Project = mongoose.model('Project', projectSchema, 'projects');
 
+<<<<<<< HEAD
 
+=======
+var campaignSchema = mongoose.Schema({
+    name: String,
+    steps: [],
+    owner: String,
+    email: String
+    
+}, { timestamps: true });
+const Campaign = mongoose.model('Campaign', campaignSchema, 'campaigns');
+>>>>>>> parent of ef5b596... cleanup and adding to scheduling
 
 
 module.exports = {
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const path = require('path');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
+var env2 = require('env2')('.env');
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
@@ -18,8 +19,6 @@ const fs                = require('fs');
 const https             = require('https');
 const mysql             = require('mysql');
 const cors              = require('cors');
-
-
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -32,8 +31,7 @@ app.use(express.static('public'))
 mongoose.connect('mongodb://three:four44@ds251548.mlab.com:51548/hexboard',{useMongoClient: true});
 require('./config/passport')(passport); // pass passport for configuration
 
-
-
+// set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
@@ -41,8 +39,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
-
-
+// required for passport
 app.use(session({
     secret: 'weneedtocomeupwithsometypeofpasscode', // session secret
     resave: true,
@@ -52,7 +49,9 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
+// routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
+// launch ======================================================================
 app.listen(port);
 console.log('The magic happens on port ' + port);
